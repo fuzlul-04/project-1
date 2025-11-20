6153cefdf4a7f5336a68851755bbf72abba5211f
Fuzlul Haque <fazlul5788@gmail.com>
2025-11-20 10:02:24 +0600

    first commit

---
+++ porject-1.py
@@ -0,0 +1,11 @@
+# Mad Libs Game project
+
+username = input("what is your name:")
+fathername = input("what is your father name:")
+mothername = input("what is your mother namea:")
+study = input("which class do you read in:")
+relationship = input("tell me about your reletionship status:")
+
+print(f"My Doggy Name is {username}, My doggy father name {fathername}, my doggy mother name {mothername}"
+f"my do doggy study in class {study}, our reletionship is {relationship}")
+
